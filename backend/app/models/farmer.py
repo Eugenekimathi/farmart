251d205da401e55d894bbbba940fd6243cb54aa5
Eugene Kimathi <eugenekimathi@gmail.com>
2026-08-19 20:59:30 +0300

    Add a farmer class model

---
+++ backend/app/models/farmer.py
@@ -0,0 +1,33 @@
+from app.extensions import db
+
+class Farmer(db.Model):
+    __tablename__ = "farmers"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey("users.id"),
+        nullable=False,
+        unique=True
+    )
+
+    farm_name = db.Column(
+        db.String(150),
+        nullable=False
+    )
+
+    farm_location = db.Column(
+        db.String(150),
+        nullable=False
+    )
+
+    description = db.Column(
+        db.Text,
+        nullable=True
+    )
+
+    user = db.relationship(
+        "User",
+        backref=db.backref("farmer", uselist=False)
+    )
