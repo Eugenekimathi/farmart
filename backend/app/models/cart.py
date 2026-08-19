10f12683beaf50c791d0a258ce16942961dcbdcf
Eugene Kimathi <eugenekimathi@gmail.com>
2026-08-19 21:06:44 +0300

    Add a cart model

---
+++ backend/app/models/cart.py
@@ -0,0 +1,25 @@
+from app.extensions import db
+from datetime import datetime
+
+class Cart(db.Model):
+    __tablename__ = "carts"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey("users.id"),
+        nullable=False,
+        unique=True
+    )
+
+    created_at = db.Column(
+        db.DateTime,
+        default=datetime.utcnow
+    )
+
+    updated_at = db.Column(
+        db.DateTime,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow
+    )
