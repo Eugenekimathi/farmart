9e7ab37ed615b3c9804daa6f062a3da71c4fefa5
abere <trinityfelistas@gmail.com>
2026-08-20 12:25:12 +0300

    fix:animals endpoints

--- backend/app/routes/animals.py
+++ backend/app/routes/animals.py
@@ -1,1 +1,20 @@
+from flask import Blueprint, request, jsonify
 
+from app.extensions import db
+from app.models.animal import Animal
+from app.schemas.animal_schema import (
+    AnimalSchema,
+    AnimalResponseSchema
+)
+
+animal_bp = Blueprint(
+    "animals",
+    __name__,
+    url_prefix="/api/animals"
+)
+
+schema = AnimalSchema()
+response_schema = AnimalResponseSchema()
+many_response_schema = AnimalResponseSchema(
+    many=True
+)
