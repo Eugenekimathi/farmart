f9a159e4a52ba3ae0aba8e64a2c3570a857940e9
Eugene Kimathi <eugenekimathi@gmail.com>
2026-08-22 16:49:31 +0300

    Add animal-image schema

---
+++ backend/app/schemas/animal_image_schema.py
@@ -0,0 +1,36 @@
+from marshmallow import fields, validate
+from flask_marshmallow import Marshmallow
+
+ma = Marshmallow()
+
+
+class AnimalImageSchema(ma.Schema):
+
+    animal_id = fields.Integer(
+        required=True
+    )
+
+    image_url = fields.String(
+        required=True,
+        validate=validate.Length(
+            min=1,
+            max=500
+        )
+    )
+
+    is_primary = fields.Boolean(
+        load_default=False
+    )
+
+
+class AnimalImageResponseSchema(ma.Schema):
+
+    id = fields.Integer()
+
+    animal_id = fields.Integer()
+
+    image_url = fields.String()
+
+    is_primary = fields.Boolean()
+
+    created_at = fields.DateTime()
