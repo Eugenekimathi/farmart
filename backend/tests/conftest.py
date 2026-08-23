52ef6a6e11f3541977de59743ddfdda4df6cff04
Eugene Kimathi <eugenekimathi@gmail.com>
2026-08-23 22:54:01 +0300

    Implement test isolated database configuration

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -6,13 +6,11 @@ from app.extensions import db
 
 @pytest.fixture
 def app():
-    app = create_app("testing")
-
-    app.config.update(
-        TESTING=True,
-        SQLALCHEMY_DATABASE_URI="sqlite:///:memory:",
-        SQLALCHEMY_TRACK_MODIFICATIONS=False,
-    )
+    app = create_app({
+        "TESTING": True,
+        "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:",
+        "SQLALCHEMY_TRACK_MODIFICATIONS": False,
+    })
 
     with app.app_context():
         db.create_all()
